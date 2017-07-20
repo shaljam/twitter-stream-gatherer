16b114b6a9f22a842bef9fcf5bfb5235b6214e40
Ali Momen Sani <ali.momensany@gmail.com>
2017-07-20 11:27:57 +0430

    enable compression and wait_on_rate_limit

--- twitter.py
+++ twitter.py
@@ -21,7 +21,7 @@ load_config()
 auth = tweepy.OAuthHandler(config['consumer_key'], config['consumer_secret'])
 auth.set_access_token(config['access_token_key'], config['access_token_secret'])
 
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
 
 myStreamListener = MyStreamListener.MyStreamListener()
 myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
