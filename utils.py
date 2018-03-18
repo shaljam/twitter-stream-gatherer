6a7fcaf9f47b51a4cdb687a686a23332dc0c203b
Ali Momen Sani <ali.momensany@gmail.com>
2018-03-18 22:56:25 +0330

    better date format

--- utils.py
+++ utils.py
@@ -1,7 +1,18 @@
-from datetime import datetime, timezone
+from datetime import datetime
+from pytz import timezone, UTC
 
 
-def beautiful_now():
-    return datetime.today()\
-        .replace(tzinfo=timezone.utc).astimezone(tz=None)\
-        .strftime('%A, %b %d, %Y %H:%M:%S')
+def beautiful_now(tehran=True):
+    date = datetime.today()
+
+    if tehran:
+        date = date.replace(tzinfo=timezone('Asia/Tehran'))
+        tz = 'Tehran'
+    else:
+        date = date.replace(tzinfo=UTC)
+        tz = 'UTC'
+
+    return date.strftime('%a %b %d %Y %H:%M:%S ' + tz)
+
+
+print(beautiful_now())
